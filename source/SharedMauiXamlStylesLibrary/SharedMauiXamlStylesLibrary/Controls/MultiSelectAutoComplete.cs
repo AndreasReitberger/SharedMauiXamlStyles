b4d623160920585f42ecbbc1cf3b58995c0e6b64
Andreas <andy.reitberger@gmail.com>
2022-10-01 17:45:19 +0200

    Added new input controls
    Added new Syncfusion input controls
    Replaced StaticResource with DynamicResource for the PrimaryColor

---
+++ source/SharedMauiXamlStylesLibrary/SharedMauiXamlStylesLibrary/Controls/MultiSelectAutoComplete.cs
@@ -0,0 +1,52 @@
+﻿using Syncfusion.Maui.Inputs;
+using System.Collections;
+using SelectionChangedEventArgs = Syncfusion.Maui.Inputs.SelectionChangedEventArgs;
+
+namespace AndreasReitberger.Shared.Controls
+{
+    public class MultiSelectAutoComplete : SfAutocomplete
+    {
+        public MultiSelectAutoComplete()
+        {
+            SelectionChanged -= MultiSelectAutoComplete_SelectionChanged;
+            SelectionChanged += MultiSelectAutoComplete_SelectionChanged;
+        }
+
+        void MultiSelectAutoComplete_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection is not IList list)
+            {
+                // Single item (if token is set to 'none')
+                list = new ArrayList
+                {
+                    e.CurrentSelection
+                };
+            }
+            SelectedItemsList = list;
+        }
+
+        public IList SelectedItemsList
+        {
+            get => (IList)GetValue(SelectedItemsListProperty);
+            set => SetValue(SelectedItemsListProperty, value);
+        }
+
+        public static readonly BindableProperty SelectedItemsListProperty =
+            BindableProperty.Create(nameof(SelectedItemsList),
+                typeof(IList),
+                typeof(MultiSelectAutoComplete),
+                new ArrayList(),
+                BindingMode.Default,
+                null,
+                OnSelectionChanged);
+
+        static void OnSelectionChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            //MultiSelectAutoComplete control = bindable as MultiSelectAutoComplete;
+            if (bindable is MultiSelectAutoComplete control && newValue is IList selectedItems)
+            {
+                control.SetValue(SelectedItemsListProperty, selectedItems);
+            }
+        }
+    }
+}
