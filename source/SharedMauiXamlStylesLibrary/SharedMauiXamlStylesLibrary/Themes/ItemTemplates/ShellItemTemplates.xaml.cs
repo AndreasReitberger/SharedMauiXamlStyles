f85bc49f5306121711c47a18e3e17a7e5100bbd5
Andreas <andy.reitberger@gmail.com>
2022-12-06 10:16:13 +0100

    Added `Style` for the `Shell.ItemTemplate`
    Added `Style` for the `Shell.ItemTemplate`

---
+++ source/SharedMauiXamlStylesLibrary/SharedMauiXamlStylesLibrary/Themes/ItemTemplates/ShellItemTemplates.xaml.cs
@@ -0,0 +1,9 @@
+namespace AndreasReitberger.Shared.Themes.ItemTemplates;
+
+public partial class ShellItemTemplates : ResourceDictionary
+{
+    public ShellItemTemplates()
+    {
+        InitializeComponent();
+    }
+}
