f0e964258d0e18320dab7b15babe05209ecd476f
Andreas <andy.reitberger@gmail.com>
2024-02-09 11:17:10 +0100

    Further adjustments

---
+++ src/SharedMauiXamlStylesLibrary.Syncfusion/Resources/Themes/SharedConverters.xaml.cs
@@ -0,0 +1,9 @@
+namespace AndreasReitberger.Shared.Syncfusion;
+
+public partial class SharedConverters
+{
+    public SharedConverters()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/SharedMauiXamlStylesLibrary/Resources/Themes/SharedConverters.xaml.cs
@@ -0,0 +1,9 @@
+namespace AndreasReitberger.Shared;
+
+public partial class SharedConverters
+{
+    public SharedConverters()
+    {
+        InitializeComponent();
+    }
+}
