8d3813672ae05d3ba2174b1f9b401aedd20f141c
Andreas <andy.reitberger@gmail.com>
2025-10-09 15:36:22 +0200

    Added `SfCircularProgressBar` styles
    Fixed #700

---
+++ src/SharedMauiXamlStylesLibrary.SampleApp/ViewModels/SfStepProgressBarViewModel.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Maui.Storage;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Syncfusion.Maui.ProgressBar;
+using System.Collections.ObjectModel;
+
+namespace SharedMauiXamlStylesLibrary.SampleApp.ViewModels
+{
+    public partial class SfStepProgressBarViewModel : BaseViewModel
+    {
+        #region Properties
+
+        [ObservableProperty]
+        public partial ObservableCollection<StepProgressBarItem> StepProgress { get; set; } = 
+        [
+            new () { PrimaryText = "Packaged"},
+            new () { PrimaryText = "Shipped"},
+            new () { PrimaryText = "Delivered"},
+        ];
+
+        [ObservableProperty]
+        public partial int SelectedIndex { get; set; } = 0;
+        #endregion
+
+        #region Constructor, LoadSettings
+
+        public SfStepProgressBarViewModel(IDispatcher dispatcher, IServiceProvider provider, IFileSaver fileSaver) : base(dispatcher, provider, fileSaver)
+        {
+            Dispatcher = dispatcher;
+        }
+
+        #endregion
+
+        #region LifeCycle
+
+        public async Task OnAppearing()
+        {
+            IsBusy = true;
+            await Task.Delay(5000);
+            IsBusy = false;
+        }
+
+        #endregion
+    }
+}
