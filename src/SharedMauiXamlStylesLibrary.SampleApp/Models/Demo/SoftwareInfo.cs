25353adbb11fbafb9845e028d3fb7ee45d4c2912
Andreas <andy.reitberger@gmail.com>
2025-10-09 11:10:06 +0200

    Added `SfTreeMap` styles and samples
    Fixed #698

---
+++ src/SharedMauiXamlStylesLibrary.SampleApp/Models/Demo/SoftwareInfo.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SharedMauiXamlStylesLibrary.SampleApp.Models.Demo
+{
+    public partial class SoftwareInfo : ObservableObject
+    {
+        #region Properties
+
+        [ObservableProperty]
+        public partial int Users { get; set; }
+
+        [ObservableProperty]
+        public partial string Name { get;set; } = string.Empty;
+
+        [ObservableProperty]
+        public partial string Group { get; set; } = string.Empty;
+
+        [ObservableProperty]
+        public partial string PersonInCharge { get; set; } = string.Empty;
+
+        [ObservableProperty]
+        public partial string Website { get; set; } = string.Empty;
+        #endregion
+    }
+}
---
+++ src/SharedMauiXamlStylesLibrary.SampleApp/ViewModels/SfTreeMapPadPageViewModel.cs
@@ -0,0 +1,88 @@
+﻿using CommunityToolkit.Maui.Storage;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using SharedMauiXamlStylesLibrary.SampleApp.Models.Demo;
+using Syncfusion.Maui.SignaturePad;
+using System.Collections.ObjectModel;
+
+namespace SharedMauiXamlStylesLibrary.SampleApp.ViewModels
+{
+    public partial class SfTreeMapPadPageViewModel : BaseViewModel
+    {
+        #region Properties
+
+        [ObservableProperty]
+        public partial ObservableCollection<SoftwareInfo> Tools { get; set; } = [
+            new() { Name = "Microsoft 365", Users = 2613, Group = "Office"},
+            new() { Name = "Microsoft Teams", Users = 3156, Group = "Office"},
+            new() { Name = "SAP", Users = 841, Group = "Office"},
+            new() { Name = "Visual Studio", Users = 566, Group = "IDE"},
+            new() { Name = "Visual Studio Code", Users = 862, Group = "IDE"},
+            new() { Name = "Eclipse", Users = 1586, Group = "IDE"},
+            ];
+
+        [ObservableProperty]
+        public partial ObservableCollection<SoftwareInfo> SelectedSoftwares { get; set; } = [];
+        partial void OnSelectedSoftwaresChanged(ObservableCollection<SoftwareInfo> value)
+        {
+
+        }
+        #endregion
+
+        #region Constructor, LoadSettings
+
+        public SfTreeMapPadPageViewModel(IDispatcher dispatcher, IServiceProvider provider, IFileSaver fileSaver) : base(dispatcher, provider, fileSaver)
+        {
+            Dispatcher = dispatcher;
+        }
+
+        #endregion
+
+        #region Commands
+
+        [RelayCommand]
+        static Task DrawCompleted(object? parameter)
+            => Shell.Current.DisplayAlert("Draw completed", "Draw completed event triggered.", "OK");
+
+        [RelayCommand]
+        static Task DrawStarted(object? parameter)
+            => Shell.Current.DisplayAlert("Draw started", "Draw started event triggered.", "OK");
+
+        [RelayCommand]
+        static void Clear(object? parameter)
+        {
+            if (parameter is SfSignaturePad pad)
+            {
+                pad.Clear();
+            }
+        }
+
+        [RelayCommand]
+        async Task Save(object? parameter)
+        {
+            if (parameter is SfSignaturePad pad)
+            {
+                var background = pad.Background;
+                var foreground = pad.StrokeColor;
+
+                pad.Background = Brush.White;
+                pad.StrokeColor = Colors.Black;
+
+                IScreenshotResult? captureResult = await pad.CaptureAsync();
+                if (captureResult is not null)
+                {
+                    using MemoryStream memStream = new();
+                    await captureResult.CopyToAsync(memStream, ScreenshotFormat.Png, 100);
+                    if (FileSaver is not null)
+                    _ = await FileSaver.SaveAsync("signature.png", memStream);
+                }
+                ImageSource? imageSource = pad.ToImageSource();
+
+                pad.Background = background;
+                pad.StrokeColor = foreground;
+            }
+        }
+
+        #endregion
+    }
+}
