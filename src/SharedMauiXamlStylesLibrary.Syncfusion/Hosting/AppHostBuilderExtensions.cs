9b2a16225976782591b9b18adf335ca3c3df3276
Andreas <andy.reitberger@gmail.com>
2026-04-08 07:55:10 +0200

    Finalized PR

--- src/SharedMauiXamlStylesLibrary.Syncfusion/Hosting/AppHostBuilderExtensions.cs
+++ src/SharedMauiXamlStylesLibrary.Syncfusion/Hosting/AppHostBuilderExtensions.cs
@@ -1,6 +1,7 @@
 ﻿#if WINDOWS
 using Microsoft.Maui.Handlers;
 #endif
+using AndreasReitberger.Shared.Hosting;
 using Syncfusion.Maui.Core.Hosting;
 
 namespace AndreasReitberger.Shared.Syncfusion.Hosting
@@ -10,6 +11,7 @@ namespace AndreasReitberger.Shared.Syncfusion.Hosting
         public static MauiAppBuilder InitializeSharedSyncfusionStyles(this MauiAppBuilder builder)
         {
             builder
+                .InitializeSharedMauiStyles()
                 .RegisterSharedSyncfusionFonts()
                 .ConfigureSyncfusionAddons()
                 ;
